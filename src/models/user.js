49c2248a434c92e2f8b6fc0c4493568a1a47d1aa
Alex Tanasov <o.tanasov@do-it.co>
2018-09-19 16:29:24 +0300

    Added working registartion and login
    - bcrypt for hashing passwords 
    - added some fields to `User` model

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,22 @@
 const Sequelize = require('sequelize')
 
 exports.userFactory = sequelize => sequelize.define('User', {
-  email: {type: Sequelize.STRING, unique: true, allowNull: false},
-  password: {type: Sequelize.STRING, allowNull: false},
-  firstName: Sequelize.STRING,
-  lastName: Sequelize.STRING
+  email: { type: Sequelize.STRING, unique: true, allowNull: false },
+  password: { type: Sequelize.STRING, allowNull: false },
+  firstName: { type: Sequelize.STRING },
+  lastName: { type: Sequelize.STRING },
+  middleName: { type: Sequelize.STRING },
+  role: { type: Sequelize.STRING },
+  title: { type: Sequelize.STRING },
+  image: { type: Sequelize.STRING },
+  ssn: { type: Sequelize.STRING },
+  phone: { type: Sequelize.STRING },
+  cell: { type: Sequelize.STRING },
+  countryCode: { type: Sequelize.STRING },
+  city: { type: Sequelize.STRING },
+  website: { type: Sequelize.STRING },
+  address: { type: Sequelize.STRING },
+  state: { type: Sequelize.STRING }
 }, {
   freezeTableName: true
 })
