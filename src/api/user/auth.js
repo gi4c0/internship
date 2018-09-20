d2782e9b6625cc43c6c634e423293efa8eb1a839
Alex Tanasov <o.tanasov@do-it.co>
2018-09-20 13:35:15 +0300

    Added registration fields

--- src/api/user/auth.js
+++ src/api/user/auth.js
@@ -10,10 +10,27 @@ const saltRounds = 10
 
 exports.registerSchema = Joi.object().keys({
   email: Joi.string().email().required(),
+  password: Joi.string().min(8).required(),
+  firstName: Joi.string(),
+  lastName: Joi.string(),
+  middleName: Joi.string(),
+  title: Joi.string(),
+  image: Joi.string(),
+  ssn: Joi.string(),
+  phone: Joi.string(),
+  cell: Joi.string(),
+  countryCode: Joi.string(),
+  city: Joi.string(),
+  website: Joi.string(),
+  address: Joi.string(),
+  state: Joi.string()
+})
+exports.loginSchema = Joi.object().keys({
+  email: Joi.string().email().required(),
   password: Joi.string().min(8).required()
 })
 
-exports.register = async (req, res, next) => {
+exports.register = wrapper(async (req, res, next) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
     await User.create({ ...req.body, password: hashedPassword })
@@ -24,7 +41,7 @@ exports.register = async (req, res, next) => {
     }
     next(err)
   }
-}
+})
 
 // TODO Change "secret" on RSA key(see jsonwebtoken doc)
 exports.login = wrapper(async (req, res, next) => {
